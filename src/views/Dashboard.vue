310bb3f9593bcff775f9a6096b48307c61adf18b
robertojmm <robertojmunozdeleon@gmail.com>
2020-09-23 21:04:02 +0200

    style(dashboard): Changed app bar height

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,8 +3,10 @@
     <div id="main-container">
       <drawer :open.sync="drawer" :items="drawerItems" />
       <div class="container">
-        <v-app-bar app clipped-left color="indigo" dark>
-          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar app dense clipped-left color="indigo" dark>
+          <v-app-bar-nav-icon
+            @click.stop="drawer = !drawer"
+          ></v-app-bar-nav-icon>
           <v-toolbar-title>Mein-Manga</v-toolbar-title>
         </v-app-bar>
         <div id="content">
@@ -17,24 +19,24 @@
   </v-app>
 </template>
 <script lang="ts">
-import Vue from "vue";
-import Drawer from "@/components/Drawer.vue";
+import Vue from 'vue';
+import Drawer from '@/components/Drawer.vue';
 
 export default Vue.extend({
-  name: "Dashboard",
+  name: 'Dashboard',
   data: () => {
     return {
       drawer: true,
       drawerItems: [
-        { title: "Home", icon: "mdi-home", route: "/" },
+        { title: 'Home', icon: 'mdi-home', route: '/' },
         {
-          title: "Admin",
-          icon: "mdi-lock-open",
-          route: "/admin",
+          title: 'Admin',
+          icon: 'mdi-lock-open',
+          route: '/admin',
           requiresAdmin: true,
         },
-        { title: "Settings", icon: "mdi-cog", route: "/settings" },
-        { title: "About", icon: "mdi-information", route: "/about" },
+        { title: 'Settings', icon: 'mdi-cog', route: '/settings' },
+        { title: 'About', icon: 'mdi-information', route: '/about' },
       ],
     };
   },
